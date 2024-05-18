b913eb9953b1b03c73d26f43b9a8b24cde48c88f
tyhan99 <32393171+tyhan99@users.noreply.github.com>
2024-05-18 14:05:25 +0000

    240518-server.js 1.0009

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Needed for dotenv
+require("dotenv").config();
+
 const express = require('express');
 const { Client } = require('pg'); // Postgres client library
 
@@ -7,11 +10,23 @@ const port = process.env.PORT || 5432; // Use environment variable for port
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
+// Needed for public directory
+app.use(express.static(__dirname));
+
+// Needed for parsing form data
+app.use(express.json());       
+app.use(express.urlencoded({extended: true}));
 
 // Database connection details (replace with your actual values)
 const url = process.env.DATABASE_URL; // Replace with your actual Render URL
 
-const client = new Client({ connectionString: url });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 
 client.connect()
   .then(() => console.log('Connected to PostgreSQL database'))
