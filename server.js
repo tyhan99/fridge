dd6a254a65a0ea9ec9c13959d712763b3726d780
tyhan99 <32393171+tyhan99@users.noreply.github.com>
2024-05-23 09:14:28 +0000

    240523-server.js 1.19

--- server.js
+++ server.js
@@ -10,20 +10,16 @@ const port = process.env.PORT || 5432; // Use environment variable for port
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
-// Needed for parsing JSON data
-const { bodyParser } = require('body-parser'); 
-
 // Needed for public directory
 app.use(express.static(__dirname));
 //app.use(express.static("views"));
-
 
 // Needed for parsing form data
 app.use(express.json());       
 app.use(express.urlencoded({extended: true}));
 
-// Database connection details (replace with your actual values)
-const url = process.env.DATABASE_URL; // Replace with your actual Render URL
+// Database connection details
+const url = process.env.DATABASE_URL;
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -32,17 +28,14 @@ const client = new Client({
   }
 });
 
-
 client.connect()
   .then(() => console.log('Connected to PostgreSQL database'))
   .catch(err => console.error('Connection error:', err.stack));
 
 // Index page
 app.get('/', function(req, res) {
-
     res.render('index');
 });
-
 
 app.get('/user-ids', async (req, res) => {
   try {
@@ -57,7 +50,6 @@ app.get('/user-ids', async (req, res) => {
     res.status(500).send('Error retrieving user data');
   }
 });
-
 
 app.get('/fridge-ids', async (req, res) => {
   try {
@@ -75,41 +67,27 @@ app.get('/fridge-ids', async (req, res) => {
 
 app.post('/adduser', async (req, res) => {
   try {
-    //const input = req.body()
     const { user_id, email } = req.body;
     const result = await client.query("INSERT INTO users(user_id, email) VALUES ($1, $2)", [user_id, email]); 
     console.log(result); // Debugging
-
   } catch (err) {
     console.error('Error adding new user:', err.stack);
     res.status(500).send('Error adding new user');
   }
-
   res.redirect('/');
-
 });
 
 app.post('/addfridge', async (req, res) => {
   try {
-    //const input = req.body()
     const { fridge_name, selectedUserID } = req.body;
     const result = await client.query("INSERT INTO fridge(fridge_name, user_id) VALUES ($1, $2)", [fridge_name, selectedUserID]); 
     console.log(result); // Debugging
-
   } catch (err) {
     console.error('Error adding new fridge:', err.stack);
     res.status(500).send('Error adding new fridge');
   }
   res.redirect('/');
-
 });
-
-// new code from 21/5
-
-// app.get('/food-data', async (req, res) => {
-// old code replaced with new codes below
-// });
-
 
 app.post('/delete-food', async (req, res) => {
   try {
@@ -129,26 +107,11 @@ app.post('/add-food', async (req, res) => {
     const result = await client.query("INSERT INTO food(food_name, qty, expiry_date, note, discard, fridge_id, owner) VALUES ($1, $2, $3, $4, $5, $6, $7)", [food_name, qty, expiry_date, note, discard, fridge_id, owner]); 
     console.log(result); // Debugging
     res.json({ status: 'success' });
-      // Clear the form inputs
-        $('#food-name').val('');
-        $('#qty').val('');
-        $('#expiry-date').val('');
-        $('#note').val('');
-        $('#discard').prop('checked', false);
   } catch (err) {
     console.error('Error adding new food item:', err.stack);
     res.status(500).send('Error adding new food item');
   }
 });
-
-// app.post('/edit-food', async (req, res) => {
-// replaced with new codes below
-//});
-
-
-// end new code 21/5
-
-// begin new code 23/5
 
 app.get('/food-data', async (req, res) => {
   try {
@@ -193,8 +156,5 @@ app.post('/edit-food', async (req, res) => {
     res.status(500).send('Error editing food item');
   }
 });
-
-
-// end new code 23/5
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
